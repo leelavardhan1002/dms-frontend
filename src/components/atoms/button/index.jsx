0e6ac92298fa88a30048517eb743123f676a5229
Boddapalli Leelavardhan <leelavardhan9@gmail.com>
2024-07-27 16:28:28 +0530

    added custom button with react icons and custom icons

--- src/components/atoms/button/index.jsx
+++ src/components/atoms/button/index.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 
-const CustomButton = ({
-  text,
-  onClick,
-  btnColor,
-  btnHeight,
-  btnWidth,
-  textColor,
-}) => {
+const CustomButton = ({ text, onClick, icon, className = "" }) => {
   return (
-    <button
-      onClick={onClick}
-      className={`${btnColor} ${btnHeight} ${btnWidth} ${textColor}`}
-    >
-      {text}
+    <button onClick={onClick} className={`custom-button ${className}`}>
+      {icon && <span className={`button-icon mr-2`}>{icon}</span>}
+      {text && <span className="button-text">{text}</span>}
     </button>
   );
 };
